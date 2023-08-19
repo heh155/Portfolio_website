c43007047b5393a65419c89f5aade520e9e9166f
Asutosh <2129060@kiit.ac.in>
2023-08-19 12:59:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let section = document.querySelector('section');
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll=()=>{
+    section.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offset-150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top>=offset && top<offset+height){
+            navLinks.forEach(links=>{
+                links.classlist.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classlist.add('active');
+            });
+        };
+    });
+    let header =document.querySelector('header');
+    header.classList.toggle('sticky',windo.scrollY>100);
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+   
+};
+
+// toggle
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+ScrollReveal({ 
+    // reset: true,
+    distance:'80px',
+    duration: 2000,
+    delay:200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
+ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contactme form', { origin:'bottom' });
+ScrollReveal().reveal('.home-content h1,.about-img', { origin:'left' });
+ScrollReveal().reveal('.about-content', { origin:'right' });
